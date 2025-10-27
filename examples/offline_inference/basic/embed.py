a307ea0bba784acddf985c6dec13b6edbf18ebb2
Mr-Sorander <30584718+Mr-Sorander@users.noreply.github.com>
2025-10-27 13:57:17 +0800

    调整代码以符合vllm0.10.0版本
    1、runner=“pooling” -> task="embedding";
    2、测试了llm.embed(prompts) 和llm.encode(prompts)写法的不同
    3、增加一行注解：如果不用parser去解析命令行参数，可以直接使用llm=LLM(model="...",task="...")更简洁
    
    Signed-off-by: Mr-Sorander <30584718+Mr-Sorander@users.noreply.github.com>

--- examples/offline_inference/basic/embed.py
+++ examples/offline_inference/basic/embed.py
@@ -13,7 +13,8 @@ def parse_args():
     # Set example specific arguments
     parser.set_defaults(
         model="intfloat/e5-small",
-        runner="pooling",
+        # runner="pooling",         # vllm在0.4.0版本以后废除该参数，使用task参数。task=“embedding”表示嵌入，默认task=“generate”
+        task="embedding"
         enforce_eager=True,
     )
     return parser.parse_args()
@@ -33,19 +34,32 @@ def main(args: Namespace):
     llm = LLM(**vars(args))
 
     # Generate embedding. The output is a list of EmbeddingRequestOutputs.
-    outputs = llm.embed(prompts)
+    outputs = llm.embed(prompts) # 两种写法，返回的对象不同，详见下面的代码
+    outputs_v2 = llm.encode(prompts)
 
     # Print the outputs.
     print("\nGenerated Outputs:\n" + "-" * 60)
     for prompt, output in zip(prompts, outputs):
-        embeds = output.outputs.embedding
+        embeds = output.outputs.embedding  # 【调用 llm.embed(prompts)时，返回的是 EmbeddingOutput，取嵌入向量需要用 .embedding，返回的是list类型
+                                           #  eg: EmbeddingRequestOutput(request_id='0', outputs=EmbeddingOutput(hidden_size=1024), prompt_token_ids=[9707, 11, 847, 829, 374, 151643], finished=True)】
+        
         embeds_trimmed = (
             (str(embeds[:16])[:-1] + ", ...]") if len(embeds) > 16 else embeds
         )
         print(f"Prompt: {prompt!r} \nEmbeddings: {embeds_trimmed} (size={len(embeds)})")
         print("-" * 60)
 
-
+    print(f"="*80)
+    print("\nGenerated Outputs_v2:\n" + "-" * 60)
+    for prompt, output in zip(prompts, outputs_v2):
+        embeds = output.outputs.data    # 【调用 llm.encode(prompts)时，返回的是 PoolingOutput，取嵌入向量需要用 .data，返回的是tensor类型
+                                        #   eg: PoolingRequestOutput(request_id='4', outputs=PoolingOutput(data=tensor([ 0.0209, -0.0456, -0.0100,  ..., -0.0190, -0.0290,  0.0064])), prompt_token_ids=[9707, 11, 847, 829, 374, 151643], finished=True)】
+        embeds_trimmed = (
+            (str(embeds[:16])[:-1] + ", ...]") if len(embeds) > 16 else embeds
+        )
+        print(f"Prompt: {prompt!r} \nEmbeddings: {embeds_trimmed} (size={len(embeds)})")
+        print("-" * 60)
+        
 if __name__ == "__main__":
     args = parse_args()
     main(args)
